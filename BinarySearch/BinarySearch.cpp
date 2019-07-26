629d1a26258da005250a84c0a8acc3447ad5f681
Miroslav Jandak <miroslav.jandak@monster.com>
2019-07-26 17:18:06 +0200

    binary search

---
+++ BinarySearch/BinarySearch.cpp
@@ -0,0 +1,40 @@
+// BinarySearch.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+
+}
+
+int BinarySearch(int target, int orderedArr[], int size)
+{
+	int firstIdx = 0;
+	int lastIdx = size - 1;
+	while (firstIdx < lastIdx)
+	{
+		int middle = firstIdx + (lastIdx - firstIdx) / 2;
+		if (orderedArr[middle] == target) return middle;
+		if (target < orderedArr[middle])
+		{
+			lastIdx = middle;
+		}
+		else
+		{
+			firstIdx = middle + 1;
+		}
+	}
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
